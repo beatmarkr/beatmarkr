76321d3927a472180d2fd16d2162fe39f01838e7
Alina Chen <achen1267@ucla.edu>
2015-05-01 22:36:59 -0700

    Implemented storage system and API for bookmarks and songs.

--- app/src/main/java/com/cs130/beatmarkr/MusicSQLiteHelper.java
+++ app/src/main/java/com/cs130/beatmarkr/MusicSQLiteHelper.java
@@ -7,10 +7,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by Alina on 4/23/2015.
- * This is a Singleton class.
+ * This is a Singleton class. In using this class, when calling database modification functions,
+ * you must call
+ * helper.close();
+ * Where helper is your reference to MusicSQLiteHelper.getInstance(). The database will be open upon
  */
 
-public class MusicSQLiteHelper extends SQLiteOpenHelper {
+public class MusicSQLiteHelper extends SQLiteOpenHelper implements Storage {
 
     private static MusicSQLiteHelper helper;
     private static SQLiteDatabase db;
@@ -28,9 +31,12 @@ public class MusicSQLiteHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     //might need synchronize or something for thread safety
-    public static MusicSQLiteHelper getMusicSQLiteHelper(Context context) {
+    public static MusicSQLiteHelper getInstance(Context context) {
         if (helper == null) {
             helper = new MusicSQLiteHelper(context);
+            db = helper.getWritableDatabase();
+        }
+        if (!db.isOpen()) {
             db = helper.getWritableDatabase();
         }
         return helper;
@@ -42,14 +48,16 @@ public class MusicSQLiteHelper extends SQLiteOpenHelper {
                 "CREATE TABLE " + MusicDBContract.MusicEntry.TABLE_NAME + " (" +
                         MusicDBContract.MusicEntry.COLUMN_MUSIC_ID + INT_TYPE + " PRIMARY KEY," +
                         MusicDBContract.MusicEntry.COLUMN_TITLE + TEXT_TYPE + COMMA +
-                        MusicDBContract.MusicEntry.COLUMN_ARTIST + TEXT_TYPE + COMMA +
+                        MusicDBContract.MusicEntry.COLUMN_ARTIST + TEXT_TYPE +
                 " )";
         String SQL_CREATE_BOOKMARKS_TABLE =
                 "CREATE TABLE " + MusicDBContract.BookmarkEntry.TABLE_NAME + " (" +
-                        MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + INT_TYPE + " PRIMARY KEY," +
+                        MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + INT_TYPE + COMMA +
                         MusicDBContract.BookmarkEntry.COLUMN_DESC + TEXT_TYPE + COMMA +
                         MusicDBContract.BookmarkEntry.COLUMN_TIME + INT_TYPE + COMMA +
-                        " )";
+                        " PRIMARY KEY (" + MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + COMMA +
+                                           MusicDBContract.BookmarkEntry.COLUMN_TIME + " )" +
+                " )";
         db.execSQL(SQL_CREATE_MUSIC_TABLE);
         db.execSQL(SQL_CREATE_BOOKMARKS_TABLE);
     }
@@ -62,38 +70,169 @@ public class MusicSQLiteHelper extends SQLiteOpenHelper {
         //onCreate(db);
     }
 
-    /** Call this function when you're done writing. */
+    /** Call this function when you're done with the database.
+     *  This will invalidate any open Cursors. */
     @Override
     public synchronized void close() {
         if (helper != null)
             db.close();
     }
 
+    /** Modification functions ********************************************/
+    /*  These require a writable database. */
     public void addMusicEntry(Song song) {
-
         ContentValues values = new ContentValues();
         values.put(MusicDBContract.MusicEntry.COLUMN_MUSIC_ID, song.getID());
         values.put(MusicDBContract.MusicEntry.COLUMN_TITLE, song.getTitle());
         values.put(MusicDBContract.MusicEntry.COLUMN_ARTIST, song.getArtist());
 
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId;
-        newRowId = db.insert(MusicDBContract.MusicEntry.TABLE_NAME,null,values);
+        // Insert the new row
+        db.insert(MusicDBContract.MusicEntry.TABLE_NAME,null,values);
     }
 
-    public void deleteMusicEntry(Song song) {
-        //TODO
+    /** Deleting a song also deletes all its bookmarks. */
+    public void deleteMusicEntry(long songId) {
+        deleteAllBookmarksForSong(songId);
+
+        String where = MusicDBContract.MusicEntry.COLUMN_MUSIC_ID + " = ?";
+        String[] whereArgs = { String.valueOf(songId) };
+        db.delete(MusicDBContract.MusicEntry.TABLE_NAME, where, whereArgs);
     }
 
     public void addBookmarkEntry(Bookmark bm) {
-        //TODO
+        ContentValues values = new ContentValues();
+        values.put(MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID, bm.getMusicID());
+        values.put(MusicDBContract.BookmarkEntry.COLUMN_TIME, bm.getSeekTime());
+        values.put(MusicDBContract.BookmarkEntry.COLUMN_DESC, bm.getDescription());
+
+        // Insert the new row
+        db.insert(MusicDBContract.BookmarkEntry.TABLE_NAME,null,values);
     }
 
-    public void updateBookmarkEntry(Bookmark bm) {
-        //TODO
+    /** User can change seekTime and/or description only. Put null for unchanged values. */
+    public void updateBookmarkEntry(Bookmark bmOld, Long newSeekTime, String newDesc) {
+        ContentValues values = new ContentValues();
+        if (newSeekTime != null) {
+            values.put(MusicDBContract.BookmarkEntry.COLUMN_TIME, newSeekTime);
+        }
+        if (newDesc != null) {
+            values.put(MusicDBContract.BookmarkEntry.COLUMN_DESC, newDesc);
+        }
+
+        String where = MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + " = ? AND " +
+                MusicDBContract.BookmarkEntry.COLUMN_TIME + " = ?";
+        String[] whereArgs = { String.valueOf(bmOld.getMusicID()), String.valueOf(bmOld.getSeekTime()) };
+
+        db.update(MusicDBContract.BookmarkEntry.TABLE_NAME, values, where, whereArgs);
     }
 
     public void deleteBookmarkEntry(Bookmark bm) {
-        //TODO
+        String where = MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + " = ? AND " +
+                       MusicDBContract.BookmarkEntry.COLUMN_TIME + " = ?";
+        String[] whereArgs = { String.valueOf(bm.getMusicID()), String.valueOf(bm.getSeekTime()) };
+        db.delete(MusicDBContract.BookmarkEntry.TABLE_NAME, where, whereArgs);
+    }
+
+    public void deleteAllBookmarksForSong(long songId) {
+        String where = MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + " = ?";
+        String[] whereArgs = { String.valueOf(songId) };
+        db.delete(MusicDBContract.BookmarkEntry.TABLE_NAME, where, whereArgs);
+    }
+
+    /** Query functions ********************************************/
+    /*  These require a readable database. */
+
+    /** Search songs by song title keywords.
+     *  Null keywords will return all songs. */
+    public Cursor queryMusic(String [] keywords) {
+        String[] projection = { //return values
+                MusicDBContract.MusicEntry.COLUMN_MUSIC_ID,
+                MusicDBContract.MusicEntry.COLUMN_TITLE,
+                MusicDBContract.MusicEntry.COLUMN_ARTIST
+        };
+
+        String sortOrder =
+                MusicDBContract.MusicEntry.COLUMN_TITLE + " DESC";
+
+        String where = null;
+        if (keywords.length > 0) {
+            where = "";
+            for (int i = 0; i < keywords.length - 1; i++) {
+                where += MusicDBContract.MusicEntry.COLUMN_TITLE + " LIKE ?% AND ";
+            }
+            where += MusicDBContract.MusicEntry.COLUMN_TITLE + " LIKE ?%";
+        }
+
+        Cursor c = db.query(
+                MusicDBContract.MusicEntry.TABLE_NAME,    // The table to query
+                projection,                               // The columns to return
+                where,                                    // The columns for the WHERE clause
+                keywords,                                 // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        return c;
+    }
+
+    /** Search bookmarks by song and description.
+     *  First string in descWords must be the musicID of the song. The rest are words in the
+     *  description.
+     *  If there is only 1 element in descWords, it will return all bookmarks for that song. */
+    public Cursor queryBookmarks(String [] descWords) {
+        String[] projection = { //return values
+                MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID,
+                MusicDBContract.BookmarkEntry.COLUMN_TIME,
+                MusicDBContract.BookmarkEntry.COLUMN_DESC
+        };
+
+        String sortOrder =
+                MusicDBContract.BookmarkEntry.COLUMN_TIME + " DESC";
+
+        String where = MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + " = ?";
+        if (descWords.length > 1) {
+            for (int i = 1; i < descWords.length; i++) {
+                where += " AND " + MusicDBContract.BookmarkEntry.COLUMN_DESC + " LIKE ?%";
+            }
+        }
+
+        Cursor c = db.query(
+                MusicDBContract.BookmarkEntry.TABLE_NAME,    // The table to query
+                projection,                               // The columns to return
+                where,                                    // The columns for the WHERE clause
+                descWords,                                 // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        return c;
+    }
+
+    /** Returns all bookmarks with a seekTime in the range [start, end] for a given song. */
+    public Cursor queryBookmarksByRange(long musicId, long start, long end) {
+        String[] projection = { //return values
+                MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID,
+                MusicDBContract.BookmarkEntry.COLUMN_TIME,
+                MusicDBContract.BookmarkEntry.COLUMN_DESC
+        };
+
+        String sortOrder =
+                MusicDBContract.BookmarkEntry.COLUMN_TIME + " DESC";
+
+        String where = MusicDBContract.BookmarkEntry.COLUMN_MUSIC_ID + " = ? AND " +
+                       MusicDBContract.BookmarkEntry.COLUMN_TIME + " >= ? AND " +
+                       MusicDBContract.BookmarkEntry.COLUMN_TIME + "<= ?";
+        String [] whereArgs = {String.valueOf(musicId), String.valueOf(start), String.valueOf(end)};
+
+        Cursor c = db.query(
+                MusicDBContract.BookmarkEntry.TABLE_NAME,    // The table to query
+                projection,                               // The columns to return
+                where,                                    // The columns for the WHERE clause
+                whereArgs,                                 // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+        return c;
     }
 }
--- app/src/main/java/com/cs130/beatmarkr/SongListActivity.java
+++ app/src/main/java/com/cs130/beatmarkr/SongListActivity.java
@@ -1,19 +1,44 @@
 package com.cs130.beatmarkr;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class SongListActivity extends ActionBarActivity {
-
+    private ArrayList<Song> songList; //List of songs displayed in alphabetical order
+    private ListView songView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
+
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+
+        getSongList(); //see function for To Do
+
+        // Sort the songs alphabetically
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
     }
 
 
@@ -42,5 +67,40 @@ public class SongListActivity extends ActionBarActivity {
     public void selectSong(View view) {
         Intent intent = new Intent(this, BookmarksActivity.class);
         startActivity(intent);
+    }
+
+
+    /** Called each time the app is opened. It updates the database's songs by getting the list of
+     * songs (audio media) on the device. It also populates songList.
+     * TODO: add/delete songs from database based on current songs on device + multithreading
+     * As of now, it will try to add songs each time (database will reject existing entries
+     * by music ID).
+     */
+
+    public void getSongList() {
+
+        Storage helper = MusicSQLiteHelper.getInstance(getApplicationContext());
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            // Get columns
+            int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+            // Add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                helper.addMusicEntry(new Song(thisId, thisTitle, thisArtist));
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+            musicCursor.close();
+            helper.close();
+        }
     }
 }
